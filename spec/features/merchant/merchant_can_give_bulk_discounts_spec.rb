f7b7cb36dae0dcf09b3ccb4b11902322aa04723b
Nick Lindeberg <lindebergnick@gmail.com>
2018-11-03 18:41:23 -0600

    adds functionality for subtotal to be correct

--- spec/features/merchant/merchant_can_give_bulk_discounts_spec.rb
+++ spec/features/merchant/merchant_can_give_bulk_discounts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'merchant can discount bulk items' do
   it 'discounts items in cart' do
     merchant = create(:merchant, role: 1)
-    item = create(:item, user: merchant, inventory: 10, price: 3.0)
+    item = create(:item, user: merchant, inventory: 21, price: 3.0)
 
     visit items_path
 
@@ -19,10 +19,22 @@ describe 'merchant can discount bulk items' do
     click_button("Add to Cart")
     visit carts_path
     expect(page).to have_content("Price: 3.0")
+    expect(page).to have_content("Subtotal: $27.00")
 
     click_button("Add 1")
     expect(page).to have_content("Price: 2.7")
+    expect(page).to have_content("Subtotal: $27.00")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
+    click_button("Add 1")
 
-
+    expect(page).to have_content("Price: 2.4")
   end
 end
